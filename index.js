ad1a1613707dfab21c35f51e88e1f3b75e3df513
Gabriel Claudino <gabri.claudino@hotmail.com>
2025-08-02 03:10:58 -0300

    deleting useless stuff

--- index.js
+++ index.js
@@ -59,14 +59,6 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-// DELETE
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
-
-//     response.status(204).end()
-// })
-
 // POST
 const MAX = 10000
 const generateId = () => {
@@ -98,35 +90,6 @@ app.post('/api/persons', (request, response) => {
 
     return response.json(persons)
 })
-
-
-
-// const generateId = () => {
-//     const maxId = notes.length > 0
-//         ? Math.max(...notes.map(n => n.id))
-//         : 0
-//     return maxId + 1
-// }
-
-// app.post('/api/notes', (request, response) => {
-//     const body = request.body
-
-//     if (!body.content) {
-//         return response.status(400).json({
-//             error: 'content missing'
-//         })
-//     }
-
-//     const note = {
-//         content: body.content,
-//         important: body.important || false,
-//         id: generateId(),
-//     }
-
-//     notes = notes.concat(note)
-
-//     response.json(note)
-// })
 
 
 const unknownEndpoint = (request, response) => {
